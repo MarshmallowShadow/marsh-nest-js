4487e6440bb41d274c00d676f22f00af6ebf6e1f
MarshmallowShadow <choiboseung@naver.com>
2025-05-27 16:34:12 +0900

    Feat: Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const options = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
